7827ffd7981936efa5121ac5c590a8bb96abe459
Wyulle Santos <wyullesousasantos@gmail.com>
2023-10-03 09:08:26 -0300

    third

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function calculate() {
-    const celsius = +document.querySelector('#n1').value;
+    const celsius = document.querySelector('#n1').value;
     const result = document.querySelector('#result');
 
-    const fahren = (celsius *  9/5) + 32;
+    const fahren = (+celsius *  9/5) + 32;
 
     console.log(fahren);
 
     result.innerHTML = `${celsius}°C equivalem à ${fahren}°F`;
 
-    if(celsius == 0) {
+    if(!celsius) {
         alert('Adicione um número para iniciar!');
         return result.innerHTML = 'Nada para mostrar!';
     }
